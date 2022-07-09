752ed6668240a61d7ed58f209200f1e8720d55d7
Cristian Venegas <cristian.venegas@materoceano.com>
2022-07-09 19:52:01 -0400

    Âfeat : ejercicios 1, 2, 4

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,7 @@
+// function example(a, b, c){ return a+b+c
+// }
+
+/* Asignación de una función a una variable. */
+ejemplo = function (a, b, c) {
+  return a + b + c;
+};
---
+++ assets/js/4_funcion.js
@@ -0,0 +1,21 @@
+let color = "";
+
+const pintarSegunIdObtenido = (e) => {
+  document.getElementById(e.target.id).style.backgroundColor = color;
+};
+
+const obtenerColorSegunTecla = (e) => {
+  console.log(e.keyCode);
+  if (e.keyCode === 65) {
+    color = "#F5F0BB";
+  } else if (e.keyCode === 83) {
+    color = "#C4DFAA";
+  } else if (e.keyCode === 68) {
+    color = "#90C8AC";
+  } else if (e.keyCode === 70){
+    color = "#73A9AD"
+  }
+};
+
+document.addEventListener("click", pintarSegunIdObtenido, false);
+document.addEventListener("keydown", obtenerColorSegunTecla, false);
